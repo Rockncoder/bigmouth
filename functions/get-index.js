e9f4fec057439be1c66c88a366cf409f5fbc7d5f
Troy Miles <rockncoder@gmail.com>
2021-09-08 19:50:50 -0700

    adds mustache and restaurant to html

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,7 +1,12 @@
 const fs = require("fs").promises;
+const Mustache = require('mustache');
+const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const superagent = require('superagent');
+
+const restaurantsApiRoot = process.env.restaurant_api;
+
 
 var htmlCached = '';
-
 const loadHtml = async () => {
   if (!htmlCached) {
     htmlCached = await fs.readFile('static/index.html', 'utf-8');
@@ -9,8 +14,14 @@ const loadHtml = async () => {
   return htmlCached;
 }
 
+const getRestaurants = async() => (await superagent.get(restaurantsApiRoot)).body;
+
 module.exports.handler = async function(event, context){
-  let html = await loadHtml();
+  let template = await loadHtml();
+  let restaurants = await getRestaurants();
+  const dayOfWeek = days[new Date().getDay()];
+  let html = Mustache.render(template, {dayOfWeek, restaurants});
+
   return {
     statusCode: 200,
     body: html,
